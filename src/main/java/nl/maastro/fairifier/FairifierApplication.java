d0aa31b4ed7c2a50b797405106bb34c19c6f7dff
kevin_melchers <kevin.melchers@maastro.nl>
2019-01-28 07:55:27 +0100

    Initial upload empty project

---
+++ src/main/java/nl/maastro/fairifier/FairifierApplication.java
@@ -0,0 +1,14 @@
+package nl.maastro.fairifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairifierApplication.class, args);
+	}
+
+}
+
