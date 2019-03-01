c05a8a75d5cc673b08cd9440093ab063c109dc62
peter.fick <peter.fick@maastro.nl>
2019-03-01 11:01:18 +0100

    Update swagger to version 2.8.0

--- src/main/java/nl/maastro/fairifier/config/SwaggerConfiguration.java
+++ src/main/java/nl/maastro/fairifier/config/SwaggerConfiguration.java
@@ -1,5 +1,10 @@
 package nl.maastro.fairifier.config;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -8,17 +13,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StopWatch;
+
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.Date;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -30,7 +30,7 @@ public class SwaggerConfiguration {
 
     public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
 
-    private String contactName = "Maastro Clinic";
+    private String contactName = "MAASTRO clinic";
     private String contactUrl = "http://www.maastro.nl";
     private String contactEmail;
     private String title;
@@ -39,7 +39,7 @@ public class SwaggerConfiguration {
     private String termsOfServiceUrl;
     private String license;
     private String licenseUrl;
-
+    
     /**
      * Swagger Springfox configuration.
      *
@@ -48,25 +48,17 @@ public class SwaggerConfiguration {
     @Bean
     public Docket swaggerSpringfoxDocket() {
         logger.debug("Starting Swagger");
-        StopWatch watch = new StopWatch();
-        watch.start();
-
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .forCodeGeneration(true)
                 .genericModelSubstitutes(ResponseEntity.class)
                 .ignoredParameterTypes(Pageable.class)
-                .ignoredParameterTypes(java.sql.Date.class)
-                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.LocalDate.class, Date.class)
                 .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
                 .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
                 .select()
                 .paths(regex(DEFAULT_INCLUDE_PATTERN))
                 .build();
-
-        watch.stop();
-        logger.debug("Started Swagger in {} ms", watch.getTotalTimeMillis());
-        return docket;
     }
 
     private ApiInfo apiInfo() {
@@ -88,72 +80,36 @@ public class SwaggerConfiguration {
                 contactEmail);
     }
 
-    public String getContactName() {
-        return contactName;
-    }
-
     public void setContactName(String contactName) {
         this.contactName = contactName;
-    }
-
-    public String getContactUrl() {
-        return contactUrl;
     }
 
     public void setContactUrl(String contactUrl) {
         this.contactUrl = contactUrl;
     }
 
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public String getVersion() {
-        return version;
     }
 
     public void setVersion(String version) {
         this.version = version;
     }
 
-    public String getTermsOfServiceUrl() {
-        return termsOfServiceUrl;
-    }
-
     public void setTermsOfServiceUrl(String termsOfServiceUrl) {
         this.termsOfServiceUrl = termsOfServiceUrl;
     }
 
-    public String getLicense() {
-        return license;
-    }
-
     public void setLicense(String license) {
         this.license = license;
-    }
-
-    public String getLicenseUrl() {
-        return licenseUrl;
     }
 
     public void setLicenseUrl(String licenseUrl) {
