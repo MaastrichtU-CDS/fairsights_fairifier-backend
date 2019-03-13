c2458134808f46f0a6c3a1fb38f9ec2094de8506
peter.fick <peter.fick@maastro.nl>
2019-03-13 09:18:55 +0100

    Add CORS allowed origin for localhost

---
+++ src/main/java/nl/maastro/fairifier/config/CorsConfiguration.java
@@ -0,0 +1,28 @@
+package nl.maastro.fairifier.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(CorsConfigurationProperties.class)
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    private CorsConfigurationProperties corsConfigurationProperties;
+
+    public CorsConfiguration(CorsConfigurationProperties corsConfigurationProperties) {
+        this.corsConfigurationProperties = corsConfigurationProperties;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        corsConfigurationProperties.getAllowedOrigins())
+                .allowedMethods("GET", "POST", "PUT")
+                .allowCredentials(true);
+    }
+
+}
