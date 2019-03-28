f9efae827b0df2725cb6ed02dcfa3bc92002226f
peter.fick <peter.fick@maastro.nl>
2019-03-28 18:09:36 +0100

    Add DataSourceConfiguration

---
+++ src/main/java/nl/maastro/fairifier/config/DataSourceConfiguration.java
@@ -0,0 +1,40 @@
+package nl.maastro.fairifier.config;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import nl.maastro.fairifier.config.DataSourceConfigurationProperties.DataSourceProperties;
+import nl.maastro.fairifier.services.DataSourceService;
+
+@Configuration
+@EnableConfigurationProperties(DataSourceConfigurationProperties.class)
+public class DataSourceConfiguration {
+    
+    private DataSourceService dataSourceService;
+    private DataSourceConfigurationProperties dataSourceConfigurationProperties;
+
+    public DataSourceConfiguration(
+            DataSourceService dataSourceService,
+            DataSourceConfigurationProperties dataSourceConfigurationProperties) {
+        this.dataSourceService = dataSourceService;
+        this.dataSourceConfigurationProperties = dataSourceConfigurationProperties;
+    }
+    
+    @PostConstruct
+    public void initializeDataSources() {
+        for (DataSourceProperties dataSourceProperties : dataSourceConfigurationProperties.getDataSources()) {
+            dataSourceService.addDataSource(
+                    dataSourceProperties.getName(),
+                    dataSourceProperties.getUrl(),
+                    dataSourceProperties.getDriver(),
+                    dataSourceProperties.getUsername(),
+                    dataSourceProperties.getPassword());
+        }
+    }
+    
+    
+
+
+}
