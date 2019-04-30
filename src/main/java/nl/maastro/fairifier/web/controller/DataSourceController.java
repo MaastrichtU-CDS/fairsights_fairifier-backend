8f4cb2faf95b9e9c4e60baac8ccc21fc020f57d3
peter.fick <peter.fick@maastro.nl>
2019-04-30 17:00:27 +0200

    Put DataSourceDto in separate file

--- src/main/java/nl/maastro/fairifier/web/controller/DataSourceController.java
+++ src/main/java/nl/maastro/fairifier/web/controller/DataSourceController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import nl.maastro.fairifier.domain.DatabaseDriver;
 import nl.maastro.fairifier.services.DataSourceService;
+import nl.maastro.fairifier.web.dto.DataSourceDto;
 
 @RestController
 @RequestMapping("/api")
@@ -62,61 +62,6 @@ public class DataSourceController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .header("error", e.getLocalizedMessage())
                     .build();
-        }
-    }
-    
-    public static class DataSourceDto {
-        
-        String name;
-        String url;
-        DatabaseDriver driver;
-        String username; 
-        String password;
-        
-        public String getName() {
-            return name;
-        }
-        
-        public void setName(String name) {
-            this.name = name;
-        }
-        
-        public String getUrl() {
-            return url;
-        }
-        
-        public void setUrl(String url) {
-            this.url = url;
-        }
-        
-        public DatabaseDriver getDriver() {
-            return driver;
-        }
-
-        public void setDriver(DatabaseDriver driver) {
-            this.driver = driver;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-        
-        public void setUsername(String username) {
-            this.username = username;
-        }
-        
-        public String getPassword() {
-            return password;
-        }
-        
-        public void setPassword(String password) {
-            this.password = password;
-        }
-        
-        @Override
-        public String toString() {
-            return "DataSourceDto [name=" + name + ", url=" + url + ", driver=" + driver + ", username=" + username
-                    + ", password=" + password + "]";
         }
     }
 
