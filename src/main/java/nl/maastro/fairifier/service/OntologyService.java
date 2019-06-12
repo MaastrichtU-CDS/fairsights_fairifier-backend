9e9bff56d79cec3c92870516320b3de8ecb401bc
peter.fick <peter.fick@maastro.nl>
2019-06-12 13:51:03 +0200

    Add controllers for importing ontologies

---
+++ src/main/java/nl/maastro/fairifier/service/OntologyService.java
@@ -0,0 +1,87 @@
+package nl.maastro.fairifier.service;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.RepositoryResult;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class OntologyService {
+    
+    private final Logger logger = LoggerFactory.getLogger(OntologyService.class);
+    
+    private Repository ontologyRepository;
+
+    public OntologyService(
+            @Qualifier("ontologyRepository") Repository ontologyRepository,
+            DataSourceService dataSourceService) {
+        this.ontologyRepository = ontologyRepository;
+    }
+    
+    public List<String> getOntologyBaseUris() throws Exception {
+        try (RepositoryConnection connection = ontologyRepository.getConnection()) {
+            connection.begin();
+            RepositoryResult<Resource> contexts = connection.getContextIDs();
+            List<String> ontologyBaseUris = new ArrayList<>();
+            while (contexts.hasNext()) {
+                Resource context = contexts.next();
+                ontologyBaseUris.add(context.stringValue());
+            }
+            return ontologyBaseUris;
+        } catch (RepositoryException | RDFParseException e) {
+            // Turn these runtime exceptions into a checked exception
+            throw new Exception(e);
+        }
+    }
+    
+    public void addOntology(MultipartFile file, String baseUri, RDFFormat format) throws Exception {
+        if (format == null) {
+            logger.info("No RDF format provided; trying to deduce RDF format from file extension");
+            format = Rio.getParserFormatForFileName(file.getOriginalFilename())
+                    .orElseThrow(() -> new Exception(
+                            "Unable to deduce RDF format from file name '" + file.getName() + "'"));
+            logger.info("Using RDF format: " + format);
+        }
+        
+        SimpleValueFactory factory = SimpleValueFactory.getInstance();
+        IRI context = factory.createIRI(baseUri);
+        
+        try (RepositoryConnection connection = ontologyRepository.getConnection()) {
+            connection.begin();
+            connection.add(file.getInputStream(), baseUri, format, context);
+            connection.commit();
+        } catch (RepositoryException | RDFParseException e) {
+            // Turn these runtime exceptions into a checked exception
+            throw new Exception(e);
+        }
+    }
+    
+    public void addOntology(URL url, String baseUri, RDFFormat format) throws Exception {
+        SimpleValueFactory factory = SimpleValueFactory.getInstance();
+        IRI context = factory.createIRI(baseUri);
+        try (RepositoryConnection connection = ontologyRepository.getConnection()) {
+            connection.begin();
+            connection.add(url, baseUri, format, context);        
+            connection.commit();
+        } catch (RepositoryException | RDFParseException e) {
+            // Turn these runtime exceptions into a checked exception
+            throw new Exception(e);
+        }
+    }
+    
+}
--- src/main/java/nl/maastro/fairifier/web/controller/OntologyController.java
+++ src/main/java/nl/maastro/fairifier/web/controller/OntologyController.java
@@ -1,10 +1,20 @@
 package nl.maastro.fairifier.web.controller;
 
+import java.net.URL;
+import java.util.List;
+
+import org.eclipse.rdf4j.rio.RDFFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import nl.maastro.fairifier.service.OntologyService;
 
 @RestController
 @RequestMapping("/api")
@@ -12,16 +22,60 @@ public class OntologyController {
     
     private final Logger logger = LoggerFactory.getLogger(OntologyController.class);
     
+    private OntologyService ontologyService;
+    
+    public OntologyController(OntologyService ontologyService) {
+        this.ontologyService = ontologyService;
+    }
+    
+    @PostMapping(value="/ontologies")
+    public ResponseEntity<List<String>> getOntologies() {
+        logger.info("REST request get all ontologies (as list of base URIs)");
+        try {
+            List<String> baseUris = ontologyService.getOntologyBaseUris();
+            return ResponseEntity.ok(baseUris);
+        } catch (Exception e) {
+            logger.error("Failed to get list of base URIs", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .header("errorMessage", e.getMessage())
+                    .build();
+        }
+    }
+    
     @PostMapping(value="/ontology/import/file")
-    public void importOntologyFromFile() {
+    public ResponseEntity<Void> importOntologyFromFile(
+            @RequestParam(name="file") MultipartFile file,
+            @RequestParam(name="baseUri") String baseUri,
+            @RequestParam(name="format", required=false) RDFFormat format) {
         logger.info("REST request to import ontology from file");
-        // TODO
+        try {
+            ontologyService.addOntology(file, baseUri, format);
+            logger.info("Successfully imported new ontology");
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            logger.error("Failed to import ontology", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .header("errorMessage", e.getMessage())
+                    .build();
+        }
     }
     
     @PostMapping(value="/ontology/import/url")
-    public void importOntologyFromUrl() {
+    public ResponseEntity<Void> importOntologyFromUrl(
+            @RequestParam(name="url") URL url,
+            @RequestParam(name="baseUri") String baseUri,
+            @RequestParam(name="format", required=false) RDFFormat format) {
         logger.info("REST request to import ontology from URL");
-        // TODO
+        try {
+            ontologyService.addOntology(url, baseUri, format);
+            logger.info("Successfully imported new ontology");
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            logger.error("Failed to import ontology", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .header("errorMessage", e.getMessage())
+                    .build();
+        }
     }
-
+    
 }
